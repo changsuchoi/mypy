5e6dd126a4b7f6cb544f169058c6c91dfdefc9fd
changsuchoi <changsu.choi@gmail.com>
2020-04-02 10:29:30 +0900

    upload puthdr.py

---
+++ util/phuhdr.py
@@ -0,0 +1,7 @@
+# put header info (key, value, comment)
+
+def puthdr(inim, hdrkey, hdrval, hdrcomment=''):
+	from astropy.io import fits
+	hdr		=	fits.getheader(inim)
+	fits.setval(inim, hdrkey, value=hdrval, comment=hdrcomment)
+	comment     = inim+'\t'+'('+hdrkey+'\t'+str(hdrval)+')'
